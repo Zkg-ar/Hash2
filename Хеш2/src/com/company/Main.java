84e2184208c62453fdf5647f7ad8834dd4868899
Kzg-ar <kalanderyan99@gmail.com>
2020-10-05 22:32:00 +0300

    hash

---
+++ Хеш2/src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+public class Main {
+
+    private static String hash = "";
+    private static char [] alphabet = new char[]{
+            'а','б','в','г','д','е','ё','ж',
+            'з','и','й','к','л','м','н','о',
+            'п','р','с','т','у','ф','х','ц',
+            'ч','ш','щ','ъ','ы','ь','э','ю','я'
+    };
+
+    public static void main(String[] args) {
+        System.out.println(HashFunc("угу,ага,оно,сто,ост,сон,нос.Привет,что нового?"));
+    }
+
+    public static int func(int first,int last,int sum){
+
+        int res = (int) (Math.pow(first,3)+Math.pow(last,2)+last-first)%sum;
+        return res;
+    }
+    public static String HashFunc(String input){
+        int sum = 0,first=0,last=0;
+        input = input.toLowerCase();
+        String [] words = input.split("\\s|[.!,?\\-]");
+
+        for (int i = 0;i<words.length;i++){
+            for(int j = 0;j<words[i].length();j++){
+                for (int k = 0;k<alphabet.length;k++) {
+                    if(words[i].charAt(0)==alphabet[k]){
+                        first=k;
+                    }
+                    if(words[i].charAt(words[i].length()-1)==alphabet[k]){
+                        last=k;
+                    }
+                    if (words[i].charAt(j) == alphabet[k]) {
+                        sum += k;
+                    }
+
+                }
+
+            }
+//
+            hash +=Integer.toString((int) (func(first,last,sum)))+" ";
+            sum = 0;
+
+        }
+
+        return hash;
+    }
+
+}
+
